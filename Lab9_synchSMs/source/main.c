2131e5047157ec847def9b5dff0af5705a512eb4
sidrk01 <81598654+sidrk01@users.noreply.github.com>
2021-05-14 15:32:50 -0700

    Update main.c

--- Lab9_synchSMs/source/main.c
+++ Lab9_synchSMs/source/main.c
@@ -13,12 +13,24 @@
 #include "simAVRHeader.h"
 #endif
 
+unsigned char threeLEDs;
+unsigned char blinkingLED;
+
+enum ThreeLEDsSM {SMStart, Bit0, Bit1, Bit2 } three_state;
+enum BlinkingLEDSM {SMStart1, Bit_ON, Bit_OFF } one_state;
+enum CombineLEDsSM {SMStart2, Comb_Bit } comb_state;
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRB = 0x00;    PORTB = 0x00;
     /* Insert your solution below */
+    TimerSet(1000);
+    TimerOn();
+    
     while (1) {
-
+    
+    while(!TimerFlag);
+    TimerFlag = 0;
     }
     return 1;
 }
